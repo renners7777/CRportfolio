a80f4c142064b3d2f42989aebacc160c34402164
Chris Renshaw <ChrisRenshaw7@outlook.com>
2025-04-12 17:31:59 +0100

    Removed duplication of content from HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
-import Header from '../components/Header';
 import Skills from '../components/Skills';
 import About from '../components/About';
-import Footer from '../components/Footer';
 
 const HomePage = () => {
     return (
         <div>
-            <Header />
             <main>
                 <About />
                 <Skills />
             </main>
-            <Footer />
         </div>
     );
 };
