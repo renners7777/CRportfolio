57b8639566aee3a9b3d2307f54d9025977b3752e
Chris Renshaw <ChrisRenshaw7@outlook.com>
2025-04-13 22:13:34 +0100

    Replace About with Home component plus add a service page

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomePage from './pages/HomePage';
 import PortfolioPage from './pages/PortfolioPage';
-import About from './components/Home';
+import Services from './pages/Services';
+import Home from './components/Home';
 import Contact from './components/Contact';
 import Skills from './components/Skills';
 import './styles/css/App.css';
@@ -20,7 +21,11 @@ const App = () => {
           {/* Use element prop with JSX, no exact needed */}
           <Route path="/" element={<HomePage />} />
           <Route path="/portfolio" element={<PortfolioPage />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          {/* Use element prop with JSX */}
+          {/* You can also use the same component for multiple routes if needed */}
+          <Route path="/portfolio/:id" element={<PortfolioPage />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/skills" element={<Skills />} />
           {/* Optional: Add a catch-all route for 404 Not Found */}
