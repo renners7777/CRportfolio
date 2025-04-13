7697406e908ac38206e49bee6a66a15e85ba7481
Chris Renshaw <ChrisRenshaw7@outlook.com>
2025-04-13 16:48:18 +0100

    Fix import statements in HomePage component to include file extensions

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Skills from '../components/Skills';
-import About from '../components/About';
+import Skills from '../components/Skills.jsx';
+import About from '../components/About.jsx';
 
 const HomePage = () => {
     return (
