871ce6482d849ed28f24b1ada65e9bca136c712c
Chris Renshaw <ChrisRenshaw7@outlook.com>
2025-04-16 22:11:15 +0100

    Refactor Home component to use imported profile picture and enhance Footer styles with modal and button designs

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../styles/css/Home.css'; // Import the CSS file
+import profilePic from '../assets/ProfilePic1.jpeg'; // Path relative to the component file
 
 const Home = () => {
     return (
         <section className="about-section" id="about"> {/* Added id for potential navigation */}
             <div className="container">
-                <img src="src/assets/ProfilePic1.jpeg" alt="Chris Renshaw" className="profile-pic" />
+                <img src={profilePic} alt="Profile Picture" className="profile-pic" />
                 <h2>Profile</h2>
                 <p>
                     Hi, I'm Chris Renshaw, a passionate freelance software engineer specializing in building modern, responsive web applications. I help businesses bring their ideas to life with clean code and user-friendly designs.
